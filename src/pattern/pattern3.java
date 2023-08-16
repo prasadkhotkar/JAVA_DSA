7c2872172329d8fb0a86155b80cd6ce2d1ab617e
prasad khotkar <109893694+prasadkhotkar@users.noreply.github.com>
2023-08-16 14:29:16 +0530

    patterns code added

---
+++ src/pattern/pattern3.java
@@ -0,0 +1,18 @@
+package pattern;
+
+public class pattern3 {
+    public static void main(String[] args) {
+            pattern3(5);
+    }
+    static void pattern3(int n)
+    {
+        for (int i=0;i<=n;i++)
+        {
+            for (int j=0;j<=n-i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
