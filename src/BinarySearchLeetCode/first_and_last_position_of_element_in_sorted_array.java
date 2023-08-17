51868204e881033e7260d7765e42c8eefd8b9c7e
prasad khotkar <109893694+prasadkhotkar@users.noreply.github.com>
2023-08-17 17:04:32 +0530

    infiniteArray code added

--- src/BinarySearchLeetCode/first_and_last_position_of_element_in_sorted_array.java
+++ src/BinarySearchLeetCode/first_and_last_position_of_element_in_sorted_array.java
@@ -1,7 +1,7 @@
 package BinarySearchLeetCode;
 //https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
 public class first_and_last_position_of_element_in_sorted_array {
-    public int[] searchRange(int[] nums, int target) {
+    public int[] searchRange(int[] arr, int target) {
         int start=0;
         int end=arr.length-1;
 
@@ -17,7 +17,8 @@ public class first_and_last_position_of_element_in_sorted_array {
                 end = mid - 1;
             }
         }
-        return arr[start % arr.length-1];
+//        return arr[start % arr.length];
+        return new int[]{-1};
     }
     }
-}
+
