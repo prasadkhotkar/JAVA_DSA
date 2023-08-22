790a6b1634f87fb395114bd3a4830c1db6ff03b3
prasad khotkar <109893694+prasadkhotkar@users.noreply.github.com>
2023-08-22 19:32:22 +0530

    peakIndexInMountainArray code added

---
+++ src/BinarySearchLeetCode/peakIndexInMountainArray.java
@@ -0,0 +1,25 @@
+package BinarySearchLeetCode;
+
+public class peakIndexInMountainArray {
+    public static void main(String[] args) {
+
+    }
+    // https://leetcode.com/problems/peak-index-in-a-mountain-array/submissions/
+    public int peakIndexInMountainArray(int[] arr) {
+        int start=0;
+        int end=arr.length-1;
+        while (start<end)
+        {
+            int mid=start+(end-start)/2;
+            if (arr[mid]>arr[mid+1])
+            {
+                    end=mid;
+            }
+            else{
+                start=mid+1;
+            }
+
+        }
+        return start;
+    }
+}
